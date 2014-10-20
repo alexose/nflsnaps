08b79430753d85471149c38f09314b122d983ccc
Alexander Ose <alex@alexose.com>
2014-10-19 22:06:36 -0400

    beginnings of interval stuff

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ var http   = require("http")
   , url    = require("url")
   , path   = require("path")
   , fs     = require("fs")
-  , qs     = require("querystring")
-  , Sieve  = require("sievejs");
+  , qs     = require("querystring");
 
-var args   = process.argv || [];
+var options = {
+  url : 'http://www.nfl.com/liveupdate/game-center/{{dir}}/{{gid}}_gtd.json',
+  delay : 10000
+};
+
+var args = process.argv || [];
 
 var ports = {
   http   : args[2] || 3000,
@@ -39,25 +43,45 @@ var template = fs.readFileSync('index.tmpl', 'utf8');
 // HTTP interface
 http.createServer(function(request, response) {
 
-  respond('It works');
+  var queries = qs.parse(request.url.split('?')[1])
+    , interval = {};
 
   function error(string){
     respond(string, "text", 500);
   }
 
-  // End the response
-  function finish(results){
+  if (queries.gid){
+    start(queries.gid);
+  } else {
+    explain();
+  }
 
-    var string = JSON.stringify(results)
-      , type = "text/plain";
+  function explain(){
+    respond('Please provide a Game ID (e.g. 2014101912).');
+  }
 
-    // Support JSONP
-    if (queries.callback){
-      type = "application/x-javascript";
-      string = queries.callback + '(' + string + ')';
+  function start(gid){
+    var url = options.url
+      .replace('{{dir}}', gid)
+      .replace('{{gid}}', gid);
+
+    interval[gid] = setInterval(function(){
+      fetch(url, broadcast);
+    }, options.delay);
+  }
+
+  function broadcast(response){
+    console.log(response);
+  }
+
+  function stop(gid){
+    if (interval[gid]){
+      clearInterval(interval[gid]);
+      console.log('Stopped polling gid ' + gid);
+    } else {
+      console.log('Tried to stop polling gid ' + gid + ', but it wasn\'t running.');
     }
 
-    respond(string, type);
   }
 
   function respond(string, type, code){
@@ -79,3 +103,22 @@ http.createServer(function(request, response) {
   console.log('Server running on port ' + ports.http);
 });
 
+
+function fetch(url, cb){
+
+  var request = http.request(url, function(response){
+    var body = ""
+    response.on('data', function(data) {
+      body += data;
+    });
+    response.on('end', function(){
+      cb(body);
+    });
+  });
+
+  request.on('error', function(e) {
+    console.log('Problem with request: ' + e.message);
+  });
+
+  request.end();
+}
